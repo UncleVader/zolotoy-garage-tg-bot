a9c895a3e8efb80ca9552f0a0cdb3b9163ebbac7
Олег Скакун <ovskakun@gmail.com>
2024-10-10 20:34:19 +0100

    small style fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,12 @@ export default function RootLayout({children}:{children: ReactNode}) {
       </head>
       <body>
         <Providers>
-          <div className="w-full h-full min-h-screen flex flex-col justify-between">
+          <div className="full-screen-container fixed inset-0 flex flex-col justify-between overflow-hidden">
             <Header />
-            <div className="flex-1 overflow-y-auto relative p-4">
+            <div 
+            className="flex-1 overflow-y-auto relative p-4"
+            id="appContent"
+            >
               {children}
             </div>
             <Footer />
