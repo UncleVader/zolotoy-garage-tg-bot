f37f049ed50299994fd8e2caa00dc2c6a7caed0f
Олег Скакун <ovskakun@gmail.com>
2024-10-10 16:50:32 +0100

    added new functionality

--- src/app/providers/TelegramProvider.tsx
+++ src/app/providers/TelegramProvider.tsx
@@ -5,6 +5,7 @@ import {
     bindViewportCSSVars, 
     initCloudStorage, 
     initMiniApp, 
+    initSwipeBehavior, 
     useThemeParams, 
     useViewport 
 } from "@telegram-apps/sdk-react";
@@ -16,6 +17,7 @@ export default function TelegramProvider({ children }: { children: ReactNode }) 
     const themeParams = useThemeParams();
     const [miniApp] = initMiniApp();
     const cloudStorage = initCloudStorage();
+    const [swipeBehavior] = initSwipeBehavior();
 
     useEffect(() => {
         if (viewport) {
@@ -24,8 +26,42 @@ export default function TelegramProvider({ children }: { children: ReactNode }) 
     }, [viewport]);
     
     useEffect(() => {
+        if (themeParams.bgColor) {
+            miniApp.setHeaderColor(themeParams.bgColor)
+            miniApp.setBgColor(themeParams.bgColor)
+        }
         return bindThemeParamsCSSVars(themeParams);
     }, [themeParams]);
+
+    useEffect(() => {
+        const appContent = document.getElementById('appContent');
+
+        if (!appContent || !(appContent instanceof HTMLElement)) return;
+
+        const isScrollable = () => {
+            return appContent.scrollHeight > appContent.clientHeight;
+        };
+
+        const handleTouchStart = (e: TouchEvent) => {
+            if (appContent.contains(e.target as Node) && isScrollable()) {
+                swipeBehavior.disableVerticalSwipe();
+            } else {
+                swipeBehavior.enableVerticalSwipe();
+            }
+        };
+
+        const handleTouchEnd = () => {
+            swipeBehavior.enableVerticalSwipe();
+        };
+
+        document.addEventListener('touchstart', handleTouchStart, { passive: false });
+        document.addEventListener('touchend', handleTouchEnd, { passive: false });
+
+        return () => {
+            document.removeEventListener('touchstart', handleTouchStart);
+            document.removeEventListener('touchend', handleTouchEnd);
+        };
+    }, [swipeBehavior]);
 
     useEffect(() => {
         cloudStorage.get('phone-number').then((storedPhoneNumber: string | null) => {
